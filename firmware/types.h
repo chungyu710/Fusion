0a439951fa7dd42a78dca1f3b32d41e99ef015cc
Kyle Pinto <kylepinto@yahoo.ca>
2022-06-09 00:53:08 -0400

    Fixed width integer types

---
+++ firmware/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint24_t U24;
+typedef uint32_t U32;
+
+typedef int8_t S8;
+typedef int16_t S16;
+typedef int24_t S24;
+typedef int32_t S32;
+
+#endif /* TYPES_H */
