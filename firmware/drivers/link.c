1e5e8fb3481f6e68a06f0d68afea7b9e68bd03b8
Kyle Pinto <kylepinto@yahoo.ca>
2022-07-13 01:28:59 -0400

    Link response with checksum over payload and header

--- firmware/drivers/link.c
+++ firmware/drivers/link.c
@@ -2,8 +2,22 @@
 
 #include <xc.h>
 
-void link_initialize(void);
+#include "uart.h"
+
 void link_respond(Status status, void * data, U8 length)
 {
+	Response response;
+	response.status = status;
+	response.length = length;
 
+	response.checksum = response.status ^ response.length;
+	char * bytes = (char *)data;
+
+	for (U8 i = 0; i < length; i++)
+	{
+		response.checksum ^= bytes[i];
+	}
+
+	uart_transmit(&response, sizeof(response));
+	uart_transmit(data, length);
 }
