9de5d7ec2a4232ad5479452d8f188108168c262f
Kyle Pinto <kylepinto@yahoo.ca>
2022-07-13 01:16:15 -0400

    Clean up main

--- firmware/main.c
+++ firmware/main.c
@@ -7,19 +7,9 @@
 #include "uart.h"
 #include "led.h"
 
-#include "tests/gyro.h"
-#include "tests/accel.h"
-#include "tests/led.h"
-
 void main(void)
 {
 	system_initialize();
-	//test_gyro();
-	//test_accel();
-
-	//printf("WHO_AM_I: 0x%02X\r\n", imu_read_register(WHO_AM_I));
-
-	//test_led();
 
 	while (1)
 	{
