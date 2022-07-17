1c63e5328457c4286e117ab760c7647d22a34b02
Kyle Pinto <kylepinto@yahoo.ca>
2022-07-17 00:49:09 -0400

    Measure latency and switch mouse axes

--- software/drivers/deserializer.py
+++ software/drivers/deserializer.py
@@ -1,4 +1,5 @@
 from drivers.common import *
+
 import serial
 import log
 
--- software/fusion.py
+++ software/fusion.py
@@ -1,6 +1,7 @@
 from drivers.state_lookup import *
 from drivers.deserializer import *
 from drivers.calibration_processor import *
+from time import time
 from agent import *
 import argparse
 
@@ -30,7 +31,10 @@ if __name__ == '__main__':
     agent = Agent()
 
     while True:
+        start_time = time()
         sensors = get_all_sensor_data(serial_port)
+        end_time = time()
+        print(f"latency: {end_time - start_time}")
         #sensors.accel.x = scale_sensors(100, sensors.accel.x, SENSOR_MAX_OF_ACCEL_X, SENSOR_MIN_OF_ACCEL_X, NEUTRAL_OF_ACCEL_X)
         #sensors.accel.y = scale_sensors(100, sensors.accel.y, SENSOR_MAX_OF_ACCEL_Y, SENSOR_MIN_OF_ACCEL_Y, NEUTRAL_OF_ACCEL_Y)
         #sensors.accel.z = scale_sensors(100, sensors.accel.z, SENSOR_MAX_OF_ACCEL_Z, SENSOR_MIN_OF_ACCEL_Z, NEUTRAL_OF_ACCEL_Z)
@@ -38,13 +42,15 @@ if __name__ == '__main__':
 
         #state = get_state_based_on_accel(sensors.accel)
         #print(state)
+        log.disable()
 
-        sensors.gyro.roll = scale_sensors(50, sensors.gyro.roll, 4000, -4000, 21)
-        sensors.gyro.yaw = scale_sensors(50, sensors.gyro.yaw, 4000, -4000, -37)
+        sensors.gyro.pitch = scale_sensors(50, sensors.gyro.pitch, 4000, -4000, 46)
+        sensors.gyro.roll = scale_sensors(50, sensors.gyro.roll, 4000, -4000, 22)
+        sensors.gyro.yaw = scale_sensors(50, sensors.gyro.yaw, 4000, -4000, -42)
 
-        print(sensors)
+        #print(sensors)
 
-        pyautogui.move(-sensors.gyro.roll, -sensors.gyro.yaw)
+        pyautogui.move(sensors.gyro.yaw, sensors.gyro.pitch)
         #agent.perform_action(state.lower())
 
 # code from agent
