ad9ce25e0ef3a2d4a1f56733aba1ea19bbaa9062
Kyle Pinto <kylepinto@yahoo.ca>
2022-07-17 00:49:09 -0400

    Measure latency and switch mouse axes

--- software/sample.py
+++ software/sample.py
@@ -1,6 +1,7 @@
 import argparse
 
 from drivers.deserializer import *
+from time import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,5 +13,8 @@ if __name__ == '__main__':
     serial_port = configure_and_open(args.port)
     log.disable()
     while True:
+        start_time = time()
         sensors = get_all_sensor_data(serial_port)
+        end_time = time()
+        print(f"latency: {end_time - start_time}")
         print(sensors)
