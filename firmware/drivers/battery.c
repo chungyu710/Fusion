2e24f4975558e20580432e27d324076db5ab6b6a
Kyle Pinto <kylepinto@yahoo.ca>
2022-07-18 16:20:00 -0400

    Dummy battery firmware

--- firmware/drivers/battery.c
+++ firmware/drivers/battery.c
@@ -2,8 +2,24 @@
 
 #include <xc.h>
 
-void battery_initialize(void);
+#include "system.h"
+
+#define TRIS_BATTERY    TRISA2
+#define ANSEL_BATTERY   ANS2
+
+#define LOW_BATTERY_mV   3300   // 3.3 V
+
+void battery_initialize(void)
+{
+
+}
+
 U16 battery_voltage(void)
 {
-	return 0;
+	return 69;   // change this later
+}
+
+bool battery_low(void)
+{
+	return battery_voltage() <= LOW_BATTERY_mV;
 }
--- firmware/drivers/battery.h
+++ firmware/drivers/battery.h
@@ -1,10 +1,13 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
+#include <stdbool.h>
+
 #include "types.h"
 
 void battery_initialize(void);
 U16 battery_voltage(void);   // reported in mV
+bool battery_low(void);
 
 // ADC reading is scaled to be 1023 = 5 V, so some post processing is required to convert this to a voltage.
 
