41ab56ea82809b4ea27b6b718461e831c46282eb
Kyle Pinto <kylepinto1018@gmail.com>
2023-01-27 15:27:19 -0500

    RX queue

--- firmware/main.c
+++ firmware/main.c
@@ -6,13 +6,36 @@
 #include "led.h"
 #include "battery.h"
 
+#define QUEUE_SIZE   5
+
+static U8 queue [QUEUE_SIZE];
+static int queue_read = 0;
+static int queue_write = 0;
+static int queue_length = 0;
+static bool queue_overrun = false;
+
 void main(void)
 {
 	system_initialize();
 
 	while (1)
 	{
-		// TODO: remove service from interrupt and use an RX buffer instead
+		if (queue_length > 0)
+		{
+			U8 request = queue[queue_read];
+			queue_read = (queue_read + 1) % QUEUE_SIZE;
+			queue_length--;
+
+			led_on();
+			system_service(request);
+			led_off();
+		}
+
+		if (queue_overrun)
+		{
+			system_abort();
+		}
+
 		// TODO: check battery on a periodic timer
 		if (battery_low())
 		{
@@ -25,14 +48,15 @@ void __interrupt() isr()
 {
 	if (PIR1bits.RCIF)
 	{
-		U8 request;
-		uart_receive(&request, 1);
-		led_on();
-		system_service(request);
-		led_off();
-	}
-	else
-	{
-		system_abort();
+		if (queue_length < QUEUE_SIZE)
+		{
+			queue[queue_write] = RCREG;
+			queue_write = (queue_write + 1) % QUEUE_SIZE;
+			queue_length++;
+		}
+		else
+		{
+			queue_overrun = true;
+		}
 	}
 }
